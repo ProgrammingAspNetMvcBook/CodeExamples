deb5495886bc366346d5e81b8548c1b23cd8daff
mrchief <mrchief_2000@yahoo.com>
2011-12-16 17:23:23 -0500

    Skeletal view for Product and Auction page

---
+++ Website/Models/AuctionModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ebuy;
+
+namespace Website.Models
+{
+    public class AuctionModel
+    {
+        public IEnumerable<Auction> Auctions { get; set; }
+    }
+
+    /// <summary>
+    /// View Model for an Auction
+    /// </summary>
+    public class Auction
+    {
+        public Product Product { get; set; }
+        public User Winner { get; set; }
+        public Currency BidPrice { get; set; }
+        public TimeSpan ClosingIn { get; set; }
+    }
+}
