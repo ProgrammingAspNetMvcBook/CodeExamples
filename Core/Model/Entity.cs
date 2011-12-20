b6bad2224ed9c3fcdc9db19545b109c3fc17aa8e
Jess Chadwick <jesschadwick@gmail.com>
2011-12-20 01:10:01 -0500

    Protecting Id and Key properties

--- Core/Model/Entity.cs
+++ Core/Model/Entity.cs
@@ -27,15 +27,17 @@ namespace Ebuy
 
     public abstract class Entity : IEntity
     {
-        public virtual long Id { get; set; }
+        [Key]
+        public long Id { get; protected set; }
 
         [Unique, StringLength(50)]
         public virtual string Key
         {
             get { return _key = _key ?? GenerateKey(); }
-            set { _key = value; }
+            protected set { _key = value; }
         }
         private string _key;
+
 
         protected virtual string GenerateKey()
         {
