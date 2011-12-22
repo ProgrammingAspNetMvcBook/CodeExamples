b48a7924b524ee2e2c2871681ed95f36fffe8b91
devinrader <devin@twilio.com>
2011-12-22 07:43:46 -0500

    added users infrastructure

---
+++ Website/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ebuy.Website.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+    }
+}
