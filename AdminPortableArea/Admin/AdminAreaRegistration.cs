b5faa0a21ba28dd6010aba9f5b4834ed97bb4c79
devinrader <devin@twilio.com>
2011-12-22 07:30:24 -0500

    Added Admin Area and MvcContrib package

---
+++ AdminPortableArea/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using MvcContrib.PortableAreas;
+
+namespace Website.Areas.Admin
+{
+public class AdminAreaRegistration : PortableAreaRegistration
+{
+    public override string AreaName
+    {
+        get
+        {
+            return "Admin";
+        }
+    }
+
+    public override void RegisterArea(AreaRegistrationContext context, IApplicationBus bus)
+    {            
+        context.MapRoute(
+            "Admin_default",
+            "Admin/{controller}/{action}/{id}",
+            new { action = "Index", id = UrlParameter.Optional }
+        );
+    }
+}
+}
---
+++ AdminPortableArea/Admin/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace AdminPortableArea.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /Admin/User/
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
