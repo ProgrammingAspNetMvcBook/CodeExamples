f95cc4646bfc759ab5fd0ffa89bfdbfab12ab607
devinrader <devin@twilio.com>
2011-12-28 16:04:21 -0500

    adding back the portables areas stuff

---
+++ Website/App_Start/ViewEngines.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Ebuy.Website.App_Start;
+using MvcContrib.PortableAreas;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Ebuy.Website.App_Start.ViewEngines), "Start")]
+
+namespace Ebuy.Website.App_Start
+{
+    public static class ViewEngines
+    {
+        public static void Start()
+        {
+            PortableAreaRegistration.RegisterEmbeddedViewEngine();
+        }
+    }
+}
