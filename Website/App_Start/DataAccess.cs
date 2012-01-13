a6fb67a8bb384d9f9464d254520912a507aecb46
Jess Chadwick <jesschadwick@gmail.com>
2012-01-13 00:02:15 -0500

    Stripping membership initialization into its own class

--- Website/App_Start/DataAccess.cs
+++ Website/App_Start/DataAccess.cs
@@ -1,15 +1,10 @@
 using System.Configuration;
 using System.Data.Entity;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web.Management;
-using System.Web.Security;
 using Devtalk.EF.CodeFirst;
 using Ebuy.DataAccess;
 using Ebuy.Website.App_Start;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(DataAccess), "InitializeDatabase")]
-[assembly: WebActivator.PostApplicationStartMethod(typeof(DataAccess), "CreateAdminUser")]
 
 namespace Ebuy.Website.App_Start
 {
@@ -34,30 +29,6 @@ namespace Ebuy.Website.App_Start
             using (var context = new DataContext())
             {
                 context.Database.Initialize(false);
-            }
-        }
-
-        public static void CreateAdminUser()
-        {
-            using (var context = new DataContext())
-            {
-                var admin = new User {
-                    DisplayName = "Administrator",
-                    EmailAddress = "admin@ebuy.com",
-                    Username = "admin",
-                };
-
-                if (!context.Users.Any(x => x.Key == admin.Key))
-                {
-                    context.Users.Add(admin);
-                    context.SaveChanges();
-
-                    var conn = new SqlConnectionStringBuilder(context.Database.Connection.ConnectionString);
-                    SqlServices.Install(conn.InitialCatalog, SqlFeatures.Membership | SqlFeatures.RoleManager, conn.ConnectionString);
-
-                    MembershipCreateStatus status;
-                    Membership.CreateUser(admin.Username, "Password!", admin.EmailAddress, null, null, true, null, out status);
-                }
             }
         }
     }
