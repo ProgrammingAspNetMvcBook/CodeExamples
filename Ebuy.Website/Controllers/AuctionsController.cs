8a9e7ed4847935c1b7d95aaac20e700e7c5d98a2
Jess Chadwick <jesschadwick@gmail.com>
2012-09-30 23:36:34 -0400

    Chapter 1

---
+++ Ebuy.Website/Controllers/AuctionsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ebuy.Website.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        //
+        // GET: /Auctions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Auctions/Details/5
+
+        public ActionResult Details(long id = 0)
+        {
+            var auction = new Ebuy.Website.Models.Auction
+            {
+                Id = id,
+                Title = "Brand new Widget 2.0",
+                Description = "This is a brand new version 2.0 Widget!",
+                StartPrice = 1.00m,
+                CurrentPrice = 13.40m,
+                StartTime = DateTime.Parse("6-15-2012 12:34 PM"),
+                EndTime = DateTime.Parse("6-23-2012 12:34 PM"),
+            };
+
+            return View(auction);
+        }
+        //
+        // GET: /Auctions/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Auctions/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Auctions/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Auctions/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Auctions/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Auctions/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Ebuy.Website/Models/Auction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ebuy.Website.Models
+{
+    public class Auction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal StartPrice { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
