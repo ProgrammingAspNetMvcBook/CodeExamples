78d9ea55331d5650fabb263449e35728a78fb436
Jess Chadwick <jesschadwick@gmail.com>
2012-09-30 23:35:12 -0400

    Cleanup

--- Ebuy/Website/Models/FeaturedAuctionsViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Ebuy.Website.Models
-{
-    public class FeaturedAuctionsViewModel
-    {
-        public IEnumerable<AuctionViewModel> Auctions { get; set; }
-    }
-}
--- Ebuy/build.bat
+++
@@ -1,6 +0,0 @@
-SET MSBUILD="%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-SET MSTEST="%VS100COMNTOOLS%..\IDE\mstest.exe"
-SET BUILDDIR="%CD%\Build"
-
-%MSBUILD% /t:Clean,Rebuild /p:OutDir=%BUILDDIR%/ /p:Configuration=Release "Programming ASP.NET MVC.sln"
-%MSTEST%  /testcontainer:%BUILDDIR%/UnitTests.dll /testcontainer:%BUILDDIR%/IntegrationTests.dll
