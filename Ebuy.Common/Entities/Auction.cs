35932f68966aeb116bd3be6d7bd1c81e9c8a90aa
mrchief <mrchief_2000@yahoo.com>
2013-04-10 16:06:41 -0400

    Fixed EF configuration for Auction class. Fixed #3.

--- Ebuy.Common/Entities/Auction.cs
+++ Ebuy.Common/Entities/Auction.cs
@@ -8,17 +8,26 @@ using System.Linq;
 
 namespace Ebuy
 {
-    [MetadataType(typeof(Auction.Metadata))]
     public class Auction : Entity<Guid>
     {
-        public virtual string Title { get; set; }
-        public virtual string Description { get; set; }
-        public virtual DateTime StartTime { get; set; }
-        public virtual DateTime EndTime { get; set; }
-        public virtual Currency StartPrice { get; set; }
-        public virtual Currency CurrentPrice { get; set; }
+		[Required, StringLength(500)]
+		public virtual string Title { get; set; }
 
-        public Guid? WinningBidId { get; set; }
+		[Required]
+		public virtual string Description { get; set; }
+
+		[Required]
+		public virtual DateTime StartTime { get; set; }
+
+		[Required]
+		public virtual DateTime EndTime { get; set; }
+
+		public Currency StartPrice { get; set; }
+
+		public Currency CurrentPrice { get; set; }
+
+		[ForeignKey("WinningBid")]
+		public Guid? WinningBidId { get; set; }
         public virtual Bid WinningBid { get; private set; }
 
         public bool IsCompleted
@@ -81,40 +90,7 @@ namespace Ebuy
 
             return bid;
         }
-
-
-        public class Metadata
-        {
-            [InverseProperty("Auction")]
-            public object Bids;
-
-            public object Categories;
-
-            [Required]
-            public object CurrentPrice;
-
-            [Required]
-            public object Description;
-
-            [Required]
-            public object EndTime;
-
-            [InverseProperty("Selling")]
-            public object Owner;
-
-            [Required]
-            [ForeignKey("Owner")]
-            public object OwnerId;
-
-            [Required]
-            public object StartTime;
-
-            [Required, StringLength(500)]
-            public object Title;
-
-            [ForeignKey("WinningBid")]
-            public object WinningBidId;
-        }
+        
     }
 
     public class InvalidBidException : Exception
